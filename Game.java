5c52daf06c74292484406c8f48921a2300e618f7
slate-colebank <colebas@wwu.edu>
2024-02-06 11:50:44 -0800

    init

---
+++ Game.java
@@ -0,0 +1,26 @@
+import static java.lang.System.out;
+
+import java.util.Scanner;
+public class Game {
+
+    char[] gameBoard = {' ', ' ', ' ',
+                        ' ', ' ', ' ',
+                        ' ', ' ', ' '};
+
+    public void gameStart() {
+        int gameType = chooseType();
+    }
+
+    public int chooseType() {
+        Scanner input = new Scanner(System.in);  // Create a Scanner object
+        out.println("===============================");
+        out.println("Select the AIs opponent:\n[1] Human\n [2] AI");
+       
+        System.out.println("==> ");
+
+        if (opponent.equals)
+        System.out.println("Opponent is: " + opponent);  // Output user input
+        if (opponent.e)
+        return 0;
+    }
+}
